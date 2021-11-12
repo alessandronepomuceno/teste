408d1d957a837622e17307ef8aed13d289f15a4c
alessandronepomuceno <92224382+alessandronepomuceno@users.noreply.github.com>
2021-11-11 22:10:57 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<center><br>
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	
+	
+	<title>Confirma Agenda</title>
+  </head>
+  <body>
+
+  <br><Br>
+  
+	<img src="img/confirma.png" width="300px">
+
+   <br><Br>
+
+	<div> 
+	<label for="nome">Nome Completo:</label>
+        <input type="text" id="nome" />
+    </div>
+		<div>
+			<label for="cartao">Cartão SUS:</label>
+			<input type="cartao" id="cartao" />
+		</div>
+ 
+		<div>
+			<label for="data">Data Nascimento: (D/M/A)</label>
+			<input type="data" id="data" />
+		</div>
+
+
+		<BR><BR>
+
+	<div id="liveAlertPlaceholder"></div>
+<button type="button" class="btn btn-primary" id="liveAlertBtn">Confirmar Agendamento</button>
+
+
+<script>
+
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alert(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', function () {
+    alert('Alessandro, sua consulta foi confirmada com sucesso !', 'success')
+  })
+}
+
+
+
+</script>
+
+
+
+  </body>
+</html>
+
