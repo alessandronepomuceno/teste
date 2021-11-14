3f700cf8407d2eb0d7af3df7504efac2d0feff9f
alessandronepomuceno <92224382+alessandronepomuceno@users.noreply.github.com>
2021-11-14 15:24:11 -0300

    V2 para gravação de video
    V2 para gravação de video

--- v2/index.php
+++ v2/index.php
@@ -1,51 +1,82 @@
-<<<<<<< HEAD
-<center><br>
-
-
-
 
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	
-	
-	<title>Confirma Agenda</title>
+
+    <title>Signin Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
   </head>
-  <body>
+<BR><BR>
 
-  <br><Br>
-  
-	<img src="img/confirma.png" width="300px">
 
-   <br><Br>
+    <div class="container">
 
-	<div> 
-	<label for="nome">Nome Completo:</label>
-        <input type="text" id="nome" />
-    </div>
-		<div>
-			<label for="cartao">Cartão SUS:</label>
-			<input type="cartao" id="cartao" />
-		</div>
+  <body class="text-center">
+    <form class="form-signin" action="index.php">
+      <img class="mb-4" src="img/confirma.png" alt="" width="300">
+      <h1 class="h4 mb-3 font-weight-normal">Verifique seus dados e confirme sua consulta</h1>
+      <BR><BR>
+
+      <?php
+
+class Paciente {
+	public $nome;
+	public $especialista;
+	public $data;
+	public $sus;	
+	
+	public function Confirmar() {
+		echo "<b> Paciente: </b>" .$this->nome. " <br> <b>Especialidade: </b>" .$this->especialista. " <b><br> Data Agendamento:</b> " .$this->data. " <b><br> Horário:</b> " .$this->horario. " <b><br> Unidade:</b> " .$this->local.  
+				" <!-- <br><br>Confirmou sua consulta com sucesso ! --> ";
+	}
+}
+
+
+$consulta = new Paciente();
+$consulta->nome = " João da Silva Nascimento ";
+$consulta->especialista = " Clinico Geral ";
+$consulta->data = " 18/12/1991 ";
+$consulta->horario = " 16:00 ";
+$consulta->local = " UBS JARDIM PEDROSO ";
+$consulta->sus = " 1234567890 ";
+$consulta->Confirmar ();
+
+?>
+
+
+
+
+      <div class="checkbox mb-3">
  
-		<div>
-			<label for="data">Data Nascimento: (D/M/A)</label>
-			<input type="data" id="data" />
-		</div>
+      </div>
 
 
-		<BR><BR>
+      <!-- OS DADOS DESTA PAGINA DEVEM VIR PREENCHIDOS POIS JA FORAM ADICIONADOS MA PAGINA DE CADASTRO -->
 
-	<div id="liveAlertPlaceholder"></div>
-<button type="button" class="btn btn-primary" id="liveAlertBtn">Confirmar Agendamento</button>
+
+      <BR><BR>
+      <div id="liveAlertPlaceholder"></div>
+      <button type="button" class="btn btn-primary" id="liveAlertBtn">Confirmar Agendamento</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2021</p>
+    </form>
+
+</div>
+
+
+
 
 
 <script>
@@ -62,7 +93,7 @@ function alert(message, type) {
 
 if (alertTrigger) {
   alertTrigger.addEventListener('click', function () {
-    alert('Alessandro, sua consulta foi confirmada com sucesso !', 'success')
+    alert('Sua consulta foi confirmada com sucesso !', 'success')
   })
 }
 
@@ -71,85 +102,8 @@ if (alertTrigger) {
 </script>
 
 
-
   </body>
 </html>
 
-=======
-<center><br>
 
 
-
-
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	
-	
-	<title>Confirma Agenda</title>
-  </head>
-  <body>
-
-  <br><Br>
-  
-	<img src="img/confirma.png" width="300px">
-
-   <br><Br>
-
-	<div> 
-	<label for="nome">Nome Completo:</label>
-        <input type="text" id="nome" />
-    </div>
-		<div>
-			<label for="cartao">Cartão SUS:</label>
-			<input type="cartao" id="cartao" />
-		</div>
- 
-		<div>
-			<label for="data">Data Nascimento: (D/M/A)</label>
-			<input type="data" id="data" />
-		</div>
-
-
-		<BR><BR>
-
-	<div id="liveAlertPlaceholder"></div>
-<button type="button" class="btn btn-primary" id="liveAlertBtn">Confirmar Agendamento</button>
-
-
-<script>
-
-var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
-var alertTrigger = document.getElementById('liveAlertBtn')
-
-function alert(message, type) {
-  var wrapper = document.createElement('div')
-  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
-
-  alertPlaceholder.append(wrapper)
-}
-
-if (alertTrigger) {
-  alertTrigger.addEventListener('click', function () {
-    alert('Alessandro, sua consulta foi confirmada com sucesso !', 'success')
-  })
-}
-
-
-
-</script>
-
-
-
-  </body>
-</html>
-
->>>>>>> 7c44cc9b64b59cc942c67d36d093c5102b7d4a47
